90585b4d84fb9e68e025e616fed12bfef33064e0
김진원 <48236565+Dohamsu@users.noreply.github.com>
2024-05-20 10:29:24 +0900

    도형 인지 최소크기 추가, 도형 그리기 시 템플릿 hide 추가

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,7 +1,7 @@
 import { Box } from '@mui/material';
 import React, { MouseEventHandler, useEffect, useRef, useState } from 'react';
 import { DrawOption } from '../store/type';
-import {FIGURE_LINE_WIDTH} from '../store/CONST';
+import {FIGURE_LINE_WIDTH, FIGURE_MINIMUM_SIZE} from '../store/CONST';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../store/store';
 import { saveFigure, clearFigure,  allFigures, firgureSlice } from '../store/figureSlice';
@@ -13,8 +13,11 @@ const Canvas: React.FC<DrawOption>= ({drawOption}) => {
   const [endXY, setEndXY]            = useState({ x : 0, y : 0});
   const [isDrawing, setIsDrawing]    = useState(false);
   const [fitgureList, setFigureList] = useState<any>([]);
+  const allFigures = useSelector((state: RootState) => state.figure.figureList);
+  const dispatch = useDispatch();
 
   const mouseRef     = useRef<HTMLDivElement>(null);
+  const firgureRef   = useRef<HTMLDivElement[]>([]);
   
   const figureWith   = endXY.x - startXY.x;
   const figureHeight = endXY.y - startXY.y;
@@ -22,22 +25,34 @@ const Canvas: React.FC<DrawOption>= ({drawOption}) => {
   const figureX = startXY.x - endXY.x < 0 ? startXY.x : endXY.x;
   const figureY = startXY.y - endXY.y < 0 ? startXY.y : endXY.y;
   
-  const allFigures = useSelector((state: RootState) => state.figure.figureList);
-  const dispatch = useDispatch();
   
   const stringToHTML = (element : string) => {
     let result = <div dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(element)}} />;
     return result;
   }
 
+  //전체 도형 추적
   useEffect(()=> {
     setFigureList(allFigures.map((x)=> stringToHTML(x)));
   },[allFigures]);
 
+
+  const testIsFigure = ()=>{
+    console.log( 'test');
+    console.log(figureHeight , figureWith);
+    if(Math.abs(figureHeight) < FIGURE_MINIMUM_SIZE && Math.abs(figureWith) < FIGURE_MINIMUM_SIZE){
+      return false;
+    }else{
+      return true;
+    }
+  }
+
   const mouseClickHandler: MouseEventHandler = (e) => {
     const x = e.pageX;
     const y = e.pageY;
-    
+    const svgLength = mouseRef.current?.getElementsByClassName('svgTest').length || 0;
+    const targetSvg = mouseRef.current?.getElementsByClassName('svgTest')[svgLength-1];
+
     switch(e.type){
       case 'mousedown' :   
         setStartXY({x,y});
@@ -46,15 +61,17 @@ const Canvas: React.FC<DrawOption>= ({drawOption}) => {
       break;
 
       case 'mouseup' : 
-      setIsDrawing(false);
-      let figure = e.target as Element;
-      dispatch(saveFigure(figure.outerHTML));
+        setIsDrawing(false);
+        if(testIsFigure()){
+          let figure = targetSvg as Element;
+          dispatch(saveFigure(figure.outerHTML));
+        }
       break;
 
       case 'mousemove' : 
-      if(isDrawing){
-        setEndXY({x,y});
-      }
+        if(isDrawing){
+          setEndXY({x,y});
+        }
       break;
       default: ;
     }
@@ -82,35 +99,35 @@ const Canvas: React.FC<DrawOption>= ({drawOption}) => {
           }}
           >
           <>
-          {/* { stringToHTML(allFigures[0])} */}
             {fitgureList.map((figure: any,index: any)=>
-            figure
+            <div key={index}>
+              {figure}
+            </div>
+
             )}
-          {/* {figureList.map((figure,index)=>
-                stringToHTML(figure)
-            )} */}
           </>
             <svg className='svgTest' 
-            width={Math.abs(figureWith) + 10}
-            height={Math.abs(figureHeight) + 10}
+            width={Math.abs(figureWith) }
+            height={Math.abs(figureHeight)}
             style={{
               position:'absolute',
               top: figureY,
               left: figureX,
-
+              display: isDrawing===true?  'block': 'none',
             }}>
               {drawOption==='circle'&&(
                 <ellipse 
-                cx={Math.abs(figureWith)/2} 
-                cy={Math.abs(figureHeight)/2} 
-                rx={Math.abs(figureWith/2)} 
-                ry={Math.abs(figureHeight/2)} 
-                stroke="black" 
-                strokeWidth= {FIGURE_LINE_WIDTH} 
-                fill="none" />
+                  cx={Math.abs(figureWith)/2} 
+                  cy={Math.abs(figureHeight)/2} 
+                  rx={Math.abs(figureWith/2) - FIGURE_LINE_WIDTH} 
+                  ry={Math.abs(figureHeight/2) - FIGURE_LINE_WIDTH} 
+                  stroke="black" 
+                  strokeWidth= {FIGURE_LINE_WIDTH} 
+                  fill="none" 
+                />
               )}
               {drawOption==='rec'&&(
-                <rect x={0} y={0} width={Math.abs(figureWith)} height={Math.abs(figureHeight)}  stroke="black" strokeWidth={FIGURE_LINE_WIDTH} fill="none" />
+                <rect x={0} y={0} width={Math.abs(figureWith)} height={Math.abs(figureHeight)}  stroke="black" strokeWidth={FIGURE_LINE_WIDTH +1} fill="none" />
               )}
             
             </svg>
