b64e652635bbf5b58fe207ea27a8b7f9260efb68
김진원 <48236565+Dohamsu@users.noreply.github.com>
2024-05-20 15:13:26 +0900

    도형 선택박스 구현, 도형 선택을 위한  ref추가

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,5 +1,5 @@
-import { Box } from '@mui/material';
-import React, { MouseEventHandler, useEffect, useRef, useState } from 'react';
+import { Box, Checkbox } from '@mui/material';
+import React, { MouseEventHandler, useCallback, useEffect, useRef, useState } from 'react';
 import { DrawOption } from '../store/type';
 import {FIGURE_LINE_WIDTH, FIGURE_MINIMUM_SIZE} from '../store/CONST';
 import { useDispatch, useSelector } from 'react-redux';
@@ -12,12 +12,16 @@ const Canvas: React.FC<DrawOption>= ({drawOption}) => {
   const [startXY, setStartXY]        = useState({ x : 0, y : 0});
   const [endXY, setEndXY]            = useState({ x : 0, y : 0});
   const [isDrawing, setIsDrawing]    = useState(false);
-  const [fitgureList, setFigureList] = useState<any>([]);
+  const [figureList, setFigureList] = useState<any>([]);
+  const [checkedList, setCheckedList] = useState<number[]>([]);
+  const [isChecked, setIsChecked] = useState(false);
   const allFigures = useSelector((state: RootState) => state.figure.figureList);
   const dispatch = useDispatch();
 
-  const mouseRef     = useRef<HTMLDivElement>(null);
-  const firgureRef   = useRef<HTMLDivElement[]>([]);
+  const mouseRef         = useRef<HTMLDivElement>(null);
+  const figureRef        = useRef<(SVGSVGElement | null)>(null);
+  const figureListRef    = useRef<(HTMLDivElement | null)[]>([]);
+  const figureButtonRef  = useRef<(HTMLDivElement | null)[]>([]);
   
   const figureWith   = endXY.x - startXY.x;
   const figureHeight = endXY.y - startXY.y;
@@ -33,13 +37,16 @@ const Canvas: React.FC<DrawOption>= ({drawOption}) => {
 
   //전체 도형 추적
   useEffect(()=> {
-    setFigureList(allFigures.map((x)=> stringToHTML(x)));
+    setFigureList(allFigures.map((x)=> stringToHTML(x)
+    ));
+  
+    if(figureList.length<1){
+      figureListRef.current = [];
+    }
   },[allFigures]);
 
 
   const testIsFigure = ()=>{
-    console.log( 'test');
-    console.log(figureHeight , figureWith);
     if(Math.abs(figureHeight) < FIGURE_MINIMUM_SIZE && Math.abs(figureWith) < FIGURE_MINIMUM_SIZE){
       return false;
     }else{
@@ -50,8 +57,6 @@ const Canvas: React.FC<DrawOption>= ({drawOption}) => {
   const mouseClickHandler: MouseEventHandler = (e) => {
     const x = e.pageX;
     const y = e.pageY;
-    const svgLength = mouseRef.current?.getElementsByClassName('svgTest').length || 0;
-    const targetSvg = mouseRef.current?.getElementsByClassName('svgTest')[svgLength-1];
 
     switch(e.type){
       case 'mousedown' :   
@@ -63,7 +68,8 @@ const Canvas: React.FC<DrawOption>= ({drawOption}) => {
       case 'mouseup' : 
         setIsDrawing(false);
         if(testIsFigure()){
-          let figure = targetSvg as Element;
+ 
+          let figure = figureRef.current as Element;
           dispatch(saveFigure(figure.outerHTML));
         }
       break;
@@ -77,15 +83,40 @@ const Canvas: React.FC<DrawOption>= ({drawOption}) => {
     }
   }
 
+  const checkedItemHandler = (value: number, isChecked: boolean) => {
+    if (isChecked) {
+      setCheckedList((prev) => [...prev, value]);
+      return;
+    }
+    if (!isChecked && checkedList.includes(value)) {
+      setCheckedList(checkedList.filter((item) => item !== value));
+      return;
+    }
+    return;
+  };
+
+  const checkHandler = (e: React.ChangeEvent<HTMLInputElement>, value: number) => {
+    setIsChecked(!isChecked);
+    checkedItemHandler(value, e.target.checked);
+  };
+
+  const onSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      console.log('checkedList:', checkedList);
+    },
+    [checkedList]
+  );
 
   return (
     <>
       <Box
-      width={'100%'} 
-      height={'50vh'}
-      textAlign='center' 
+        width={'100%'} 
+        height={'50vh'}
+        textAlign='center' 
       >
-          <Box 
+        <Box 
           className='svgBox'
           ref={mouseRef}
           onMouseDown={mouseClickHandler}
@@ -97,42 +128,75 @@ const Canvas: React.FC<DrawOption>= ({drawOption}) => {
           sx={{
           cursor:'pointer'
           }}
-          >
-          <>
-            {fitgureList.map((figure: any,index: any)=>
-            <div key={index}>
-              {figure}
+        >
+        <>
+          {figureList.map((figure: any,index: number)=>
+          <div 
+            key={index}
+            ref={(ref)=> 
+              figureListRef.current[index]= ref}
+              >
+            {figure}
+          </div>
+
+          )}
+        </>
+          <svg className='svgElem' 
+          width={Math.abs(figureWith) }
+          height={Math.abs(figureHeight)}
+          ref={figureRef}
+          style={{
+            position:'absolute',
+            top: figureY,
+            left: figureX,
+            display: isDrawing===true?  'block': 'none',
+          }}>
+            {drawOption==='circle'&&(
+              <ellipse 
+                cx={Math.abs(figureWith)/2 } 
+                cy={Math.abs(figureHeight)/2} 
+                rx={Math.abs(figureWith/2) > 1 ? Math.abs(figureWith/2) - FIGURE_LINE_WIDTH : Math.abs(figureWith/2)} 
+                ry={Math.abs(figureHeight/2) > 1 ? Math.abs(figureHeight/2) - FIGURE_LINE_WIDTH : Math.abs(figureHeight/2)} 
+                stroke="black" 
+                strokeWidth= {FIGURE_LINE_WIDTH} 
+                fill="none" 
+              />
+            )}
+            {drawOption==='rec'&&(
+              <rect x={0} y={0} width={Math.abs(figureWith)} height={Math.abs(figureHeight)}  stroke="black" strokeWidth={FIGURE_LINE_WIDTH +1} fill="none" />
+            )}
+          
+          </svg>
+        </Box>
+        <Box
+          sx={{
+            position:'absolute',
+            top:'105px',
+            right:'25px',
+            height:'500px',
+            width:'120px',
+            overflow:'scroll',
+            border:'1px solid black'
+          }}      
+        >
+          <div>
+            {figureList.map((figure: any,index: number)=>
+            <div 
+              key={index}
+              ref={(ref)=> 
+                figureButtonRef.current[index]= ref}
+                >
+              <Checkbox
+                checked={checkedList.includes(index)}
+                onChange={(e) => checkHandler(e, index)}
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+              />
+              <label>  layer {index}</label>
             </div>
 
             )}
-          </>
-            <svg className='svgTest' 
-            width={Math.abs(figureWith) }
-            height={Math.abs(figureHeight)}
-            style={{
-              position:'absolute',
-              top: figureY,
-              left: figureX,
-              display: isDrawing===true?  'block': 'none',
-            }}>
-              {drawOption==='circle'&&(
-                <ellipse 
-                  cx={Math.abs(figureWith)/2} 
-                  cy={Math.abs(figureHeight)/2} 
-                  rx={Math.abs(figureWith/2) - FIGURE_LINE_WIDTH} 
-                  ry={Math.abs(figureHeight/2) - FIGURE_LINE_WIDTH} 
-                  stroke="black" 
-                  strokeWidth= {FIGURE_LINE_WIDTH} 
-                  fill="none" 
-                />
-              )}
-              {drawOption==='rec'&&(
-                <rect x={0} y={0} width={Math.abs(figureWith)} height={Math.abs(figureHeight)}  stroke="black" strokeWidth={FIGURE_LINE_WIDTH +1} fill="none" />
-              )}
-            
-            </svg>
-          </Box>
-
+          </div>
+        </Box>
       </Box>
     </>
   );
