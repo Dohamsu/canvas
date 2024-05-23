572edf0bdf7cbb33ce3f4a00e33bb3542ef5d16d
김진원 <48236565+Dohamsu@users.noreply.github.com>
2024-05-23 14:49:42 +0900

    도형 reducer변경,  초기 세팅 시 테두리 삭제 추가

--- src/store/figureSlice.ts
+++ src/store/figureSlice.ts
@@ -18,9 +18,9 @@ export const firgureSlice = createSlice({
       let savedFigures = localStorage.getItem('test');
       if(savedFigures){
         let elem = savedFigures.split('drawing');
-        elem.map((x)=> 
-        // console.log(x)
-          state.figureList.push(x)
+        elem.map((x)=>
+          //저장된 테두리 삭제 
+          state.figureList.push(x.replaceAll(/border:[a-zA-Z0-9\s]*[;]/g ,''))
         )
       }
     },
@@ -28,10 +28,31 @@ export const firgureSlice = createSlice({
       state.figureList.push(action.payload);
       localStorage.setItem('test',(state.figureList.join('drawing')));
     },
-    eidtFigure: (state, action) => {
+    //이동한 도형 저장
+    editFigure: (state, action) => {
+      let newData = action.payload.split('class:')[0];
+      let newDataClass   = action.payload.split('class:')[1];
+      console.log( action.payload);
+      state.figureList.map((elem,i)=> 
+        {
+          let svgClass = elem.match(/class="[0-9]*["]/g);
+          if(svgClass){
+            let savedClass = svgClass[0].replaceAll('"','').replace('class=','');
+            if(savedClass == newDataClass){
+              state.figureList[i]= newData;
+            }
+          }          
+        }      
+      );
+      localStorage.setItem('test',(state.figureList.join('drawing')));
+    },
+    //도형 선택 삭제
+    deleteFigure: (state, action) => {
       console.log(action.payload);
       state.figureList= state.figureList.filter((v, i) => action.payload.indexOf(i)<0 );
+      localStorage.setItem('test',(state.figureList.join('drawing')));
     },
+    //도형 전체 삭제
     clearFigure: (state,action) => {
       state.figureList=[];
       localStorage.setItem('test','');
@@ -40,6 +61,6 @@ export const firgureSlice = createSlice({
   },
 });
 
-export const { initFigure, saveFigure, eidtFigure, clearFigure } = firgureSlice.actions;
+export const { initFigure, saveFigure, editFigure, deleteFigure, clearFigure } = firgureSlice.actions;
 export const allFigures = (state: RootState) => state.figure.figureList;
 export default firgureSlice.reducer;
